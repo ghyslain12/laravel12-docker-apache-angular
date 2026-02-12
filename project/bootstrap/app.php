48d5586aef42cbeca38744e2ffe0506584267bc5
[ghyslain12] <[ghyslain12@gmail.com]>
2026-02-12 08:11:21 +0100

    Add cors

--- project/bootstrap/app.php
+++ project/bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
         $middleware->alias([
             'jwt' => JwtMiddleware::class,
         ]);
